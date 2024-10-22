6308dabc80a807fb5ee8979b3bee9e3745da0bab
Peter Vu <dmtruong1980@gmail.com>
2024-10-22 18:48:08 -0500

    feat: Add HTML page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return "Hello, World!"
+    if request.method == "POST":
+        return "Hello, World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
