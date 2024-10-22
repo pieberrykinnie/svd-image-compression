fd219c53f38a95770a8ab6884656607700b04dec
Peter Vu <dmtruong1980@gmail.com>
2024-10-22 18:35:10 -0500

    feat: Add compress_image() function

--- compress_image.py
+++ compress_image.py
@@ -30,9 +30,6 @@ def svd(m: np.ndarray) -> dict:
     # Construct the singular vectors
     v: np.ndarray = ev_vectors
 
-    print(s)
-    print(v)
-
     u: np.ndarray = np.zeros((m.shape[0], m.shape[0]))
     for j in range(i):
         u[:, j] = np.matmul(m, v[:, j]) / sv[j]
@@ -47,25 +44,45 @@ def svd(m: np.ndarray) -> dict:
         for j in range(i, m.shape[0]):
             u[:, j] = ev2_vectors[:, j]
 
-    return {"S": s, "U": u, "V": v}
+    return {"S": s, "U": u, "V": v, "D": sv}
 
 def compress_image(image: Image, quality: float) -> Image:
-    """Given an image, compress it to a certain quality level given between 0 and 1."""
+    """Given an image, convert it to grayscale and compress it to a certain quality level given between 0 and 1."""
+    assert(0 <= quality and quality <= 1)
 
     # Convert the image to grayscale, then to a numpy matrix
     grayscale_image: Image = image.convert("L")
-    image_matrix: np.ndarray = np.array(grayscale_image) # apparently np.matrix is deprecated?
+    image_matrix: np.ndarray = np.array(grayscale_image)
 
     # Call the singular value decomposition function on the matrix
+    svd_result: dict = svd(image_matrix)
 
     # Use quality to determine how many singular values to keep
+    n: int = round(quality * len(svd_result["D"]))
     
-    # Reconstruct the image from the singular values matrix
+    # Construct the compressed matrix
+    u_new: np.ndarray = svd_result["U"][:, :n]
+    v_new: np.ndarray = svd_result["V"][:, :n]
 
-    # Convert the numpy matrix back to an image
+    s_new: np.ndarray = np.zeros((u_new.shape[0], v_new.shape[0]))
 
-    return image_matrix
+    for i in range(n):
+        s_new += svd_result["D"][i] * np.outer(u_new[:, i], v_new[:, i])
+        # s_new += svd_result["D"][i] * np.matmul(u_new[:, :i], v_new[:, :i].T)
 
-test_matrix: np.ndarray = np.array([[2, 0, 1], [0, 2, 0]])
-svd: dict = svd(test_matrix)
-print(np.matmul(svd["U"], np.matmul(svd["S"], svd["V"].T)))
+    # Rescale the values of the matrix to be between 0 and 1
+    if (np.min(s_new) < 0):
+        s_new -= np.min(s_new)
+    s_new /= np.max(s_new)
+
+    # Convert the numpy matrix back to a grayscale image
+    s_new = (s_new * 255).astype(np.uint8)  # Scale to 0-255 and convert to uint8
+    compressed_image: Image = Image.fromarray(s_new, mode="L")
+
+    return compressed_image
+
+# Example usage
+image: Image = Image.open("IMG_9714.jpg")
+image.convert("L").save("grayscale_image.jpg", "JPEG")
+compressed_image: Image = compress_image(image, 0.5)
+compressed_image.save("compressed_image.jpg", "JPEG")
